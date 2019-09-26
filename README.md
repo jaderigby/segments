a2c5f81394815d213079fad29b6eeb16c5d01f38
Jade Rigby <contact@jaderigby.com>
2019-09-26 13:54:50 -0600

    adding project files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Segments #
+
+## A Unique Take On Grids ##
+
+Let's face it: Humans don't pay attention to grids.  So why should our website layouts?  Introducing Segments!
+
+## We Don't Need No Stinking Grids! ##
---
+++ segments.lib.js
@@ -0,0 +1,17 @@
+class panelWrapper extends HTMLElement {}
+window.customElements.define('panel-wrapper', panelWrapper);
+
+class panelInner extends HTMLElement {}
+window.customElements.define('panel-inner', panelInner);
+
+class segmentItem extends HTMLElement {}
+window.customElements.define('segment-item', segmentItem);
+
+class segMajor extends HTMLElement {}
+window.customElements.define('x-major', segMajor);
+
+class segMinor extends HTMLElement {}
+window.customElements.define('x-minor', segMinor);
+
+class segCell extends HTMLElement {}
+window.customElements.define('x-cell', segCell);
